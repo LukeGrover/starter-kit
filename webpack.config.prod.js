3f668b17844cd1b836bfceb123e8634009141074
Luke Grover <luke.grover@vml.com>
2018-06-13 23:58:24 -0500

    Create a Production Build & Deployment Process Generate minified and gzip files via webpack, store in dist Utilizing a Separate Heroku project to host users api Utilize surge to host static dist files

---
+++ webpack.config.prod.js
@@ -0,0 +1,65 @@
+import path from 'path';
+import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import WebpackMd5Hash from 'webpack-md5-hash';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+
+export default {
+  debug: true,
+  devtool: 'source-map',
+  noInfo: false,
+  entry: {
+    vendor: path.resolve(__dirname, 'src/vendor'),
+    main: path.resolve(__dirname, 'src/index')
+  },
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: '[name].[chunkhash].js'
+  },
+  plugins: [
+    // Generate an external css file with hash
+    new ExtractTextPlugin('[name].[contenthash].css'),
+
+    //Hash the files using MD5
+    new WebpackMd5Hash(),
+
+    //UseCommonsChunkPlugin to create a separate bundle
+    // of vendor libraries so that they/re cached separately
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'
+    }),
+
+    //Create HTML file that includes reference to bundled JS
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true
+      },
+      inject: true,
+      someSpecialValue: 'A special string'
+    }),
+
+    //Eliminate duplicate packages when generating bundle
+    new webpack.optimize.DedupePlugin(),
+
+    //Minify JS
+    new webpack.optimize.UglifyJsPlugin()
+  ],
+  module: {
+    loaders: [
+      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
+      {test: /\.css$/, loader: ExtractTextPlugin.extract('css?sourceMap')}
+    ]
+  }
+}
